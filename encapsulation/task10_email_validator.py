5755a82ba392f2c4e9eda8f8d7c1693183e30fbf
yammyk <yammyk1992@gmail.com>
2023-01-29 22:58:08 +0300

    add email validator

--- encapsulation/task10_email_validator.py
+++ encapsulation/task10_email_validator.py
@@ -11,12 +11,27 @@ class EmailValidator:
 
     @classmethod
     def get_random_email(cls):
+
+        """для генерации случайного email-адреса по формату: xxxxxxx...xxx@gmail.com, где x - любой допустимый символ в
+         email (латинский буквы, цифры, символ подчеркивания и точка);"""
+
         n = randint(4, 20)
         lenght = len(cls.EMAIL_RANDOM_CHARS) - 1
         return "".join(cls.EMAIL_RANDOM_CHARS[randint(0, lenght)] for i in range(n)) + "@gmail.com"
 
     @classmethod
     def check_email(cls, email):
+
+        """возвращает True, если email записан верно и False - в противном случае.
+
+        Корректность строки email определяется по следующим критериям:
+
+        - допустимые символы: латинский алфавит, цифры, символы подчеркивания, точки и собачка @ (одна);
+        - длина email до символа @ не должна превышать 100 (сто включительно);
+        - длина email после символа @ не должна быть больше 50 (включительно);
+        - после символа @ обязательно должна идти хотя бы одна точка;
+        - не должно быть двух точек подряд."""
+
         if not cls.__is_email_str(email):
             return False
 
@@ -40,6 +55,7 @@ class EmailValidator:
 
     @staticmethod
     def __is_email_str(email):
+        """для проверки типа переменной email, если строка, то возвращается значение True, иначе - False."""
         if type(email) == str:
             return True
         else:
