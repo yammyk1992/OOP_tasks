2000c078ea3f32e7d1266717caa6f2edb2b7ab4a
yammyk <yammyk1992@gmail.com>
2023-01-29 14:01:15 +0300

    add new suggestions and new tasks

---
+++ encapsulation/task6.py
@@ -0,0 +1,29 @@
+class Book:
+
+    def __init__(self, author, title, price):
+        self.__author = author
+        self.__title = title
+        self.__price = price
+
+    def set_title(self, title):
+        self.__title = title
+
+    def set_author(self, author):
+        self.__author = author
+
+    def set_price(self, price):
+        self.__price = price
+
+    def get_title(self):
+        return self.__title
+
+    def get_author(self):
+        return self.__author
+
+    def get_price(self):
+        return self.__price
+
+
+book = Book("ВАСЯ", "ЭХ ВАСЯ", 2000)
+book.set_price(300)
+print(book.get_price())
