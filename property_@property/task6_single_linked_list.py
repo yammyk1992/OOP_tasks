cfca6223024ad4c120dbb3bee020f4dc42f3cfd4
yammyk <yammyk1992@gmail.com>
2023-02-01 22:56:43 +0300

    edit linked list methods

--- property_@property/task6_single_linked_list.py
+++ property_@property/task6_single_linked_list.py
@@ -1,8 +1,8 @@
 class StackObj:
     """для описания объектов односвязного списка;"""
 
-    def __init__(self, d):
-        self.__data = d
+    def __init__(self, data):
+        self.__data = data
         self.__next = None
 
     @property
@@ -11,7 +11,8 @@ class StackObj:
 
     @next.setter
     def next(self, n):
-        self.__next = n
+        if isinstance(n, StackObj) or n is None:
+            self.__next = n
 
     @property
     def data(self):
@@ -25,26 +26,44 @@ class StackObj:
 class Stack:
     """для управления односвязным списком."""
 
-    def __init__(self, top=None):
-        if top is not None:
-            self.top = []
+    def __init__(self):
+        self.top = None
+        self.last = None
 
     def push(self, obj):
         """ добавление объекта класса StackObj в конец односвязного списка;"""
-        self.top.append(obj.data)
+        if self.last:
+            self.last.next = obj
+
+        self.last = obj
+        if self.top is None:
+            self.top = obj
 
     def pop(self):
         """извлечение последнего объекта с его удалением из односвязного списка;"""
-        self.top.pop()
+        current_node = self.top
+        if current_node is None:
+            return
+        while current_node and current_node.next != self.last:
+            current_node = current_node.next
+        if current_node:
+            current_node.next = None
+        last = self.last
+        self.last = current_node
+        if self.last is None:
+            self.top = None
+        return last
 
     def get_data(self):
         """получение списка из объектов односвязного списка (список из строк локального атрибута __data каждого
         объекта в порядке их добавления, или пустой список, если объектов нет). """
-        return self.top
-
-
-# obj = StackObj(данные)
-# st = Stack() # создание объекта односвязного списка
+        """вывод на экран добавленных элементов"""
+        current_node = self.top
+        output = []
+        while current_node:
+            output.append(current_node.data)
+            current_node = current_node.next
+        return output
 
 
 st = Stack()
@@ -52,5 +71,4 @@ st.push(StackObj("obj1"))
 st.push(StackObj("obj2"))
 st.push(StackObj("obj3"))
 st.pop()
-res = st.get_data()  # ['obj1', 'obj2']
-print(*res)
+print(st.get_data())
